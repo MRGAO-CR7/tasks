a7e04290baccb204e427a21d90e288390882d79b
Eric Gao <eric@Erics-Macbook-Pro.local>
2020-12-08 05:32:22 +1300

    Finished the codes of the function reformat.

--- src/app/Model/Common.php
+++ src/app/Model/Common.php
@@ -8,4 +8,15 @@ final class Common
     {
         return array_merge(...array_fill(0, $times, $array));
     }
+
+    public static function reformat(String $str)
+    {
+        $result = '';
+        foreach(str_split(strtolower($str)) as $char){
+            if (in_array($char, ['a', 'e', 'i', 'o', 'u'])) continue;
+            $result .= $char;
+        }
+
+        return ucfirst($result);
+    }
 }
